abff3ad1748574450e618955bf8a41c4f22d77b8
Jan Piotrowski <janfpiotrowski@gmail.com>
2021-05-28 11:15:35 +0200

    Move State things into InitialAppState.hs

--- src/Main.hs
+++ src/Main.hs
@@ -9,37 +9,7 @@ import Actions
 import Mapping
 import DispatchOutput
 import Item 
-
-livingRoom = Room {otherRooms=["gabinet", "kuchnia", "lazienka"], roomItems=["matrioszka"], roomObjects=[], onEnterMsg="Widzisz ..."}
-office = Room {otherRooms=["salon"], roomObjects=[], roomItems=[], onEnterMsg="Widzisz ..."}
-toilet = Room {otherRooms=["salon"], roomObjects=[], roomItems=[], onEnterMsg="Widzisz ..."}
-kitchen = Room {otherRooms=["salon"], roomObjects=[], roomItems=[], onEnterMsg="Widzisz ..."}
-
-roomSet :: [(String, Room)]
-roomSet = [("salon", livingRoom),
-           ("gabinet", office),
-           ("lazienka", toilet),
-           ("kuchnia", kitchen)]
-
-matryoshka = Item {innerItem=Just "srednia matrioszka", onReadMsg="Widzisz lalke matrioszke"}
-mediumMatryoshka = Item {innerItem=Just "mala matrioszka", onReadMsg="Widzisz sredniej wielkosci lalke matrioszke"}
-smallMatryoshka = Item {innerItem=Just "bardzo mala matrioszka", onReadMsg="Widzisz mala lalke matrioszke"}
-verySmallMatryoshka = Item {innerItem=Just "stary list", onReadMsg="Widzisz bardzo mala lalke matrioszke"}
-oldLetter = Item {innerItem=Nothing, onReadMsg="Widzisz stary list z napisem: \" Jakiś napis \""}
-
-itemSet :: [(String, Item)]
-itemSet = [("matrioszka", matryoshka),
-           ("srednia matrioszka", mediumMatryoshka),
-           ("mala matrioszka", smallMatryoshka),
-           ("bardzo mala matrioszka", verySmallMatryoshka),
-           ("stary list", oldLetter)]
-
-
-
-userChar :: UserCharacter
-userChar = UserCharacter{currentRoomName="salon", inventory=[]}
-
-initialAppState = AppState {user=userChar, rooms=roomSet, items=itemSet}
+import InitialAppState
 
 action =  mapCommandToAction (GoToRoom {newRoomName="gabinet"})
 
